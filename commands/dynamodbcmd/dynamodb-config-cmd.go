29fcdbbfd54d986ccc6429034011096909c471ed
kafreen <afreen.khan@synectiks.com>
2023-09-20 17:28:06 +0530

    refactor Code

---
+++ commands/dynamodbcmd/dynamodb-config-cmd.go
@@ -0,0 +1,70 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package dynamodbcmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Appkube-awsx/awsx-common/authenticate"
+	"github.com/Appkube-awsx/awsx-common/client"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/spf13/cobra"
+)
+
+// getConfigDataCmd represents the getConfigData command
+var GetConfigDataCmd = &cobra.Command{
+	Use:   "getConfigData",
+	Short: "A brief description of your command",
+	Long:  ``,
+
+	Run: func(cmd *cobra.Command, args []string) {
+
+		authFlag, clientAuth, err := authenticate.SubCommandAuth(cmd)
+		
+		if err != nil {
+			cmd.Help()
+			return
+		}
+
+		if authFlag {
+			table, _ := cmd.Flags().GetString("table")
+			if table != "" {
+				GetDynamoDbTableDetail(table, *clientAuth)
+			} else {
+				log.Fatalln("table name not provided. program exit")
+			}
+		}
+		
+	},
+}
+
+func GetDynamoDbTableDetail(table string, auth client.Auth) (*dynamodb.DescribeTableOutput, error) {
+
+	log.Println("Getting dynamodb table data")
+
+	dynamodbClient := client.GetClient(auth, client.DYNAMODB_CLIENT).(*dynamodb.DynamoDB)
+
+	input := &dynamodb.DescribeTableInput{
+		TableName: aws.String(table),
+	}
+
+	tableData, err := dynamodbClient.DescribeTable(input)
+
+	if err != nil {
+		log.Fatalln("Error: in getting dynamodb table data", err)
+	}
+
+	log.Println(tableData)
+	return tableData, err
+}
+
+func init() {
+	GetConfigDataCmd.Flags().StringP("table", "t", "", "dynamodb table name")
+
+	if err := GetConfigDataCmd.MarkFlagRequired("table"); err != nil {
+		fmt.Println(err)
+	}
+}
--- commands/root.go
+++ commands/root.go
@@ -6,45 +6,52 @@ package commands
 import (
 	"log"
 
-	"github.com/Appkube-awsx/awsx-dynamodb/authenticater"
-	"github.com/Appkube-awsx/awsx-dynamodb/client"
+	"github.com/Appkube-awsx/awsx-common/authenticate"
+	"github.com/Appkube-awsx/awsx-common/client"
 	"github.com/Appkube-awsx/awsx-dynamodb/commands/dynamodbcmd"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/spf13/cobra"
 )
 
-// AwsxCloudElementsCmd represents the base command when called without any subcommands
+// AwsxDynamoDbCmd represents the base command when called without any subcommands
 var AwsxDynamoDbCmd = &cobra.Command{
 	Use:   "dynamodb",
 	Short: "get dynamodb Details command gets resource counts",
 	Long:  `get dynamodb Details command gets resource counts details of an AWS account`,
+
 	Run: func(cmd *cobra.Command, args []string) {
+
 		log.Println("Command dynamodb started")
-		vaultUrl := cmd.PersistentFlags().Lookup("vaultUrl").Value.String()
-		accountNo := cmd.PersistentFlags().Lookup("accountId").Value.String()
-		region := cmd.PersistentFlags().Lookup("zone").Value.String()
-		acKey := cmd.PersistentFlags().Lookup("accessKey").Value.String()
-		secKey := cmd.PersistentFlags().Lookup("secretKey").Value.String()
-		crossAccountRoleArn := cmd.PersistentFlags().Lookup("crossAccountRoleArn").Value.String()
-		externalId := cmd.PersistentFlags().Lookup("externalId").Value.String()
 
-		authFlag := authenticater.AuthenticateData(vaultUrl, accountNo, region, acKey, secKey, crossAccountRoleArn, externalId)
-
-		if authFlag {
-			getDynamoDbList(region, crossAccountRoleArn, acKey, secKey, externalId)
+		authFlag, clientAuth, err := authenticate.CommandAuth(cmd)
+		if err != nil {
+			cmd.Help()
+			return
 		}
+		if authFlag {
+			GetDynamoDbList(*clientAuth)
+		} else {
+			cmd.Help()
+			return
+		}
+		
 	},
 }
 
-func getDynamoDbList(region string, crossAccountRoleArn string, accessKey string, secretKey string, externalId string) (*dynamodb.ListTablesOutput, error) {
+func GetDynamoDbList(auth client.Auth) (*dynamodb.ListTablesOutput, error) {
+
 	log.Println("Getting dynamodb list summary")
-	dynamodbClient := client.GetClient(region, crossAccountRoleArn, accessKey, secretKey, externalId)
+
+	dynamodbClient := client.GetClient(auth, client.DYNAMODB_CLIENT).(*dynamodb.DynamoDB)
 
 	input := &dynamodb.ListTablesInput{}
+
 	tableList, err := dynamodbClient.ListTables(input)
+
 	if err != nil {
 		log.Fatalln("Error: in getting lambda list", err)
 	}
+
 	log.Println(tableList)
 	return tableList, err
 }
@@ -54,6 +61,7 @@ func getDynamoDbList(region string, crossAccountRoleArn string, accessKey string
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := AwsxDynamoDbCmd.Execute()
+
 	if err != nil {
 		log.Fatal("There was some error while executing the CLI: ", err)
 		return
@@ -64,6 +72,7 @@ func init() {
 	AwsxDynamoDbCmd.AddCommand(dynamodbcmd.GetConfigDataCmd)
 	
 	AwsxDynamoDbCmd.PersistentFlags().String("vaultUrl", "", "vault end point")
+	AwsxDynamoDbCmd.PersistentFlags().String("vaultToken", "", "vault token")
 	AwsxDynamoDbCmd.PersistentFlags().String("accountId", "", "aws account number")
 	AwsxDynamoDbCmd.PersistentFlags().String("zone", "", "aws region")
 	AwsxDynamoDbCmd.PersistentFlags().String("accessKey", "", "aws access key")
